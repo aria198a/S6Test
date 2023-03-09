876cd275fd3a115c5d1b8b4c03761ca94e08c4bc
aria198a <aria198a@gmail.com>
2023-03-09 15:30:10 +0800

    modified

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
     res.render('index')
-})
-
-app.get('/about', (req, res) => {
-    res.render('index')
-})
-
-app.get('/portfolio', (req, res) => {
-    res.render('index')
-})
-
-app.get('/contact', (req, res) => {
-    res.render('index')
-})
+  })
+  
+  app.get('/about', (req, res) => {
+    res.render('index', { showPage: 'about' })
+  })
+  
+  app.get('/portfolio', (req, res) => {
+    res.render('index', { showPage: 'portfolio' })
+  })
+  
+  app.get('/contact', (req, res) => {
+    res.render('index', { showPage: 'contact' })
+  })
 
 app.listen(port, () => {
     console.log(`Express is listening on localhost: ${port}`)
